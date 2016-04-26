c32b88805490f98a979f51cb33acef78810f96ff
rodrigoRagna <rodrigo.rivera@ciencias.unam.mx>
2016-04-26 11:49:38 -0500

    agregacion de registro y modificacion de profesor

---
+++ src/java/dao/AlumnoDao.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import org.hibernate.Session;
+
+import logica.HibernateUtil;
+import modelo.Alumno;
+
+/**
+ *
+ * @author luis
+ */
+public class AlumnoDao {
+
+    public void indroducirAlumno(String nombre, String contrasena, String correo) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Alumno n = new Alumno();
+
+        n.setSNombre(nombre);
+        n.setSContrasenha(contrasena);
+        n.setSCorreo(correo);
+
+        try {
+            session.beginTransaction();
+            session.save(n);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Error del dao de Alumno");
+            session.getTransaction().rollback();
+        }
+    }
+
+}
