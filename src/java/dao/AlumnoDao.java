f8b3ffddeb25436d8851b015a8f54a02b95539d1
Antonio Vilchis <Grand_Paladin@ciencias.unam.mx>
2016-05-01 10:48:32 -0500

    Integra los casos de uso Modificar profesor con Login, Logout y registro.

--- src/java/dao/AlumnoDao.java
+++ src/java/dao/AlumnoDao.java
@@ -8,12 +8,27 @@ package dao;
 import org.hibernate.Session;
 
 import modelo.Alumno;
+import org.hibernate.Query;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author luis
  */
 public class AlumnoDao {
+
+    private  static Session session;
+    private Transaction tx;
+    
+//    private void init(){
+//        session = HibernateUtil.getSessionFactory().getCurrentSession();
+//        tx = session.beginTransaction();
+//    }
+            
+
+    public AlumnoDao(){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
 
     public void indroducirAlumno(String nombre, String contrasena, String correo) {
 
@@ -30,9 +45,27 @@ public class AlumnoDao {
             session.save(n);
             session.getTransaction().commit();
         } catch (Exception e) {
-            System.out.println("error del dao");
+            System.out.println("Error del dao de Alumno");
             session.getTransaction().rollback();
         }
+    }
+    
+    public Alumno Verificar(Alumno alumno){
+        session.getTransaction().begin();
+        Alumno f=null;
+        try{
+     String hql= "from Alumno where s_correo= '" +alumno.getSCorreo()+"' and s_contrasenha= '"+alumno.getSContrasenha()+"'";
+     Query query=session.createQuery(hql);
+     if(!query.list().isEmpty()){
+         f=(Alumno)query.list().get(0);
+         
+     }  
+        }catch(Exception e){
+            throw e;
+        }
+        session.getTransaction().commit();
+     
+     return f;
     }
 
 }
